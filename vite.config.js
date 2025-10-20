9aaebf8f259c46b2d86ae36d3898e467c58c9f90
Joao-Matheus-Amorim <joaomatheus.lab@gmail.com>
2025-10-20 14:56:26 -0300

    Atualização da liberação do carregamento supabase

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
+    headers: {
+      'Content-Security-Policy': "default-src * 'unsafe-inline' 'unsafe-eval' data: blob:; connect-src * 'unsafe-inline'; script-src * 'unsafe-inline' 'unsafe-eval'; style-src * 'unsafe-inline'; img-src * data: blob: 'unsafe-inline'; font-src * data:; frame-src *;"
+    }
   },
 })
